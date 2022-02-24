b3f71633a0ed0c21dc79dbe2824f8455a0405f6d
Kevin Haller <kevin.haller@blockblu.io>
2022-02-24 12:22:31 +0100

    Restructures the scanner such that the calls to the db are reduced.

--- pkg/chain/syncer/scanner.go
+++ pkg/chain/syncer/scanner.go
@@ -65,41 +65,49 @@ func (sc *Scanner) scanPastBlocks(ctx context.Context) {
 
 func (sc *Scanner) newNextBlockChannel(ctx context.Context) <-chan struct{} {
 	channel := make(chan struct{})
-	go sc.next(ctx, channel, 0)
+	go sc.next(ctx, channel, 0, nil)
 	return channel
 }
 
-func (sc *Scanner) next(ctx context.Context, channel chan<- struct{}, depth int) {
+func (sc *Scanner) next(ctx context.Context, channel chan<- struct{}, depth int, block *db.AssignedBlock) {
 	wait := time.Hour
-	if depth <= 6 {
-		wait = time.Duration(math.Pow(2.0, float64(depth))) * time.Minute
-	}
-	blocks, err := sc.syncer.db.GetAssignedBlocksAfterNow(ctx)
-	success := err == nil && len(blocks) != 0
-	if success {
-		wait = blocks[0].Timestamp.Add(time.Minute).Sub(time.Now())
-		log.Infof("waiting %v for the next block", wait)
-	} else {
-		if err != nil {
-			log.Errorf("couldn't query for next block: %s", err.Error())
-		} else {
-			log.Warnf("couldn't find any next block in the database")
+	if block == nil {
+		if depth <= 6 {
+			wait = time.Duration(math.Pow(2.0, float64(depth))) * time.Minute
 		}
+		blocks, err := sc.syncer.db.GetAssignedBlocksAfterNow(ctx)
+		if err == nil && len(blocks) != 0 {
+			block = &blocks[0]
+			wait = block.Timestamp.Add(time.Minute).Sub(time.Now())
+			log.Infof("waiting %v for the next block", wait)
+		} else {
+			if err != nil {
+				log.Errorf("couldn't query for next block: %s", err.Error())
+			} else {
+				log.Warnf("couldn't find any next block in the database")
+			}
+		}
+	} else {
+		wait = block.Timestamp.Add(time.Minute).Sub(time.Now())
 	}
 	select {
 	case <-time.Tick(wait):
-		if success {
+		if block != nil {
 			pubNextBlock := func() {
 				channel <- struct{}{}
 			}
 			go pubNextBlock()
-			sc.next(ctx, channel, 0)
+			sc.next(ctx, channel, 0, nil)
 		} else {
-			sc.next(ctx, channel, depth+1)
+			sc.next(ctx, channel, depth+1, nil)
 		}
 		break
-	case <-sc.listener:
-		sc.next(ctx, channel, depth+1)
+	case msg := <-sc.listener:
+		if msg.Code == db.ObserveNewLeaderLog {
+			sc.next(ctx, channel, depth+1, nil)
+		} else {
+			sc.next(ctx, channel, depth+1, block)
+		}
 		break
 	case <-ctx.Done():
 		break
